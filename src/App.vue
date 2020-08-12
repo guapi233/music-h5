3370a37f436911108d8653dbf692aafcc81f6b41
sanSkiller <1290148953@qq.com>
2020-08-12 16:25:44 +0800

    updated:添加导航tab

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
     <router-view />
   </div>
 </template>
 
 <script>
 import MHeader from "components/m-header/m-header";
+import tab from "components/tab/tab";
 
 export default {
-  components: { MHeader }
+  components: { MHeader, tab }
 };
 </script>
 
