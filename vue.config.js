dc4f2b959ed6216b4c5885049332edac5912bfab
sanSkiller <1290148953@qq.com>
2020-08-12 16:24:36 +0800

    updated:配置代理

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,18 @@ module.exports = {
       .set("base", path.join(__dirname, "src/base"))
       .set("api", path.join(__dirname, "src/api"));
   },
+  devServer: {
+    proxy: {
+      "/api/getDiscList": {
+        target: "https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api/getDiscList": "",
+        },
+        onProxyReq(onProxyReq, req, res) {
+          onProxyReq.setHeader("referer", "https://c.y.qq.com/");
+        },
+      },
+    },
+  },
 };
