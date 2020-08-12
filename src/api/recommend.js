71602248f4bf8b5bc57fe6755fb1ee0049bf05ca
sanSkiller <1290148953@qq.com>
2020-08-12 16:27:09 +0800

    updated:推荐歌单列表请求方法

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,5 +1,6 @@
 import jsonp from "common/js/jsonp";
 import { commonParams, options } from "./config";
+import axios from "axios";
 
 export function getRecommend() {
   // QQ音乐轮播图api地址
@@ -15,4 +16,32 @@ export function getRecommend() {
 
   // 这里返回一个promise对象
   return jsonp(url, data, options);
+}
+
+/**
+ * 获取歌单数据
+ */
+export function getDiscList() {
+  const url = "/api/getDiscList";
+
+  const data = Object.assign({}, commonParams, {
+    platform: "yqq",
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random(),
+    format: "json",
+  });
+
+  return axios
+    .get(url, {
+      params: data,
+    })
+    .then((res) => {
+      // 这里返回一个Promise的resolve方法，把需要的参数传递出去
+      return Promise.resolve(res.data);
+    });
 }
