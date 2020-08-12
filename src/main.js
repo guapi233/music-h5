38db018bc43fa3ff82ae5d79450638471f1eae91
sanSkiller <1290148953@qq.com>
2020-08-12 16:29:21 +0800

    updated:配置lazyload

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueLazyLoad from "vue-lazyload";
 import fastclick from "fastclick";
 
 // 入口文件引入公共样式
@@ -9,6 +10,11 @@ import "common/stylus/index.styl";
 
 // 整个PC所有body下的dom点击都不会有300毫秒的延时
 fastclick.attach(document.body);
+
+// 安装图片懒加载插件
+Vue.use(VueLazyLoad, {
+  loading: require("common/image/default.png"),
+});
 
 Vue.config.productionTip = false;
 
