357f03fcfca6616687745c414388380735bebb26
sanSkiller <1290148953@qq.com>
2020-08-14 19:26:09 +0800

    updated:getDate:设置或者获取dom元素的data-属性

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -15,3 +15,19 @@ export function addClass(el, className) {
 export function hasClass(el, className) {
   return el.classlist.contains(className);
 }
+
+/**
+ * 设置或者获取dom元素的data-属性
+ * @param {dom} el dom
+ * @param {属性} name
+ * @param {*} val
+ */
+export function getData(el, name, val) {
+  const prefix = "data-";
+  name = prefix + name;
+  if (val) {
+    return el.setAttribute(name, val);
+  } else {
+    return el.getAttribute(name);
+  }
+}
