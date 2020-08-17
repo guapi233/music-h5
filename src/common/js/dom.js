3f1a93242f7b05ee002eefca56f02caf02f09858
sanSkiller <1290148953@qq.com>
2020-08-17 22:17:30 +0800

    updated:CSS前缀profix

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -31,3 +31,43 @@ export function getData(el, name, val) {
     return el.getAttribute(name);
   }
 }
+
+let elementStyle = document.createElement("div").style;
+
+let vendor = (() => {
+  // 各个浏览器厂商的前缀
+  let transformNames = {
+    webkit: "webkitTransform",
+    Moz: "MozTransform",
+    O: "OTransform",
+    ms: "msTransform",
+    standard: "transform",
+  };
+
+  for (const key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key;
+    }
+  }
+
+  // 如果都不满足，那这个浏览器肯定有毛病
+  return false;
+})();
+
+/**
+ * css3属性添加前缀
+ * @export
+ * @param {any} style 样式
+ * @returns 前缀+style
+ */
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false;
+  }
+
+  if (vendor === "standard") {
+    return style;
+  }
+
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1);
+}
