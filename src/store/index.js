09292a4fbbf0858362270e840431180ac537d63b
sanSkiller <1290148953@qq.com>
2020-08-17 17:58:18 +0800

    updated:拆解Vuex，方便管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as actions from "./actions";
+import * as getters from "./getters";
+import state from "./state";
+import mutations from "./mutations";
+import createLogger from "vuex/dist/logger";
 
+// vuex的应用场景： 1、多组件间的状态和数据共享； 2、路由间的复杂数据传递
 Vue.use(Vuex);
 
+// 不允许组件修改state值，只能调用mutation来进行commit
+const debug = process.env.NODE_ENV !== "production";
+
 export default new Vuex.Store({
-  state: {
-    // 歌手信息
-    singer: {},
-  },
-  mutations: {},
-  getters: {
-    singer(state) {
-      return state.singer;
-    },
-  },
-  actions: {},
-  modules: {},
+  actions,
+  getters,
+  state,
+  mutations,
+  strict: debug, // 是否允许组件修改store的数据
+  plugins: debug ? [createLogger()] : [],
 });
---
+++ src/store/state.js
@@ -0,0 +1,6 @@
+const state = {
+  // 歌手信息
+  singer: {},
+};
+
+export default state;
